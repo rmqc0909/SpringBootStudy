b51882dca4ed42a252b2184f05dd06e41a056ba1
谢丹 <rmqc0909@163.com>
2017-01-13 15:19:38 +0800

    springboot ch5_2 ch6_2_3

---
+++ ch5_2/src/main/java/com/wisely/ch5_2/Ch52Application.java
@@ -0,0 +1,12 @@
+package com.wisely.ch5_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch52Application.class, args);
+	}
+}
---
+++ ch5_2/src/test/java/com/wisely/ch5_2/Ch52ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wisely.ch5_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class Ch52ApplicationTests {
+	@Test
+	public void contextLoads() {
+	}
+}

8ff7ed92e9d4aa0308dd46ab93ed3a3e172cb3cd
谢丹 <rmqc0909@163.com>
2017-01-13 18:19:13 +0800

    springboot  ch6_2_3 加入日志

--- ch6_2_3/src/main/java/com/wisely/Ch623Application.java
+++ ch6_2_3/src/main/java/com/wisely/Ch623Application.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Ch623Application {
+
 	@Autowired
 	private AuthorSettings authorSettings;
 	public static void main(String[] args) {
